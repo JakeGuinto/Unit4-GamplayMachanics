3639e7cb2f8311046ae3eb6e7b837b8b30a7150e
JakeGuinto <116121157+JakeGuinto@users.noreply.github.com>
2023-09-21 13:34:14 -0700

    complete
    completed main game and challenge

---
+++ Unit4-GameplayMachanics/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum PowerUpType { None, Pushback, Rockets, Smash}
+public class PowerUp : MonoBehaviour
+{
+    public PowerUpType powerUpType;
+}
---
+++ Unit4-GameplayMachanics/Assets/Scripts/Spawn Manager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject[] powerupPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomPowerup = Random.Range(0, powerupPrefabs.Length);
+        Instantiate(powerupPrefabs[randomPowerup], GenerateSpawmPosition(), powerupPrefabs[randomPowerup].transform.rotation);
+        SpawnEnemyWave(waveNumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0 )
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            int randomPowerup = Random.Range(0, powerupPrefabs.Length);
+            Instantiate(powerupPrefabs[randomPowerup], GenerateSpawmPosition(), powerupPrefabs[randomPowerup].transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            int randomEnemy = Random.Range(0, enemyPrefabs.Length);
+
+            Instantiate(enemyPrefabs[randomEnemy], GenerateSpawmPosition(), enemyPrefabs[randomEnemy].transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawmPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+}
